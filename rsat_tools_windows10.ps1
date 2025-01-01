dc92b80902acedf82e7847add2dcb2851059680e
davigalucio <82713318+davigalucio@users.noreply.github.com>
2025-01-01 16:34:04 -0300

    rsat_tools_windows10.ps1

---
+++ rsat_tools_windows10.ps1
@@ -0,0 +1,10 @@
+###########################
+## RSAT Tolls Windows 10 ##
+###########################
+
+Add-WindowsCapability –online –Name Rsat.ActiveDirectory.DS-LDS.Tools~~~~0.0.1.0
+Add-WindowsCapability –online –Name Rsat.Dns.Tools~~~~0.0.1.0
+
+##############################################################################
+## Créditos: https://woshub.com/install-rsat-feature-windows-10-powershell/ ##
+##############################################################################
