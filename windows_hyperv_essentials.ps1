cf25a0f691586636b833ce75fcd2cb48dc1af339
davigalucio <82713318+davigalucio@users.noreply.github.com>
2025-01-07 05:13:04 -0300

    windows_hyperv_essentials.ps1

--- windows_hyperv_essentials.ps1
+++ windows_hyperv_essentials.ps1
@@ -3,3 +3,4 @@ Add-WindowsCapability -Name Tools.Graphics.DirectX~~~~0.0.1.0 -Online
 Add-WindowsCapability -Name NetFX3~~~~ -Online
 Add-WindowsCapability -Name Msix.PackagingTool.Driver~~~~0.0.1.0 -Online
 Add-WindowsCapability -Name Browser.InternetExplorer~~~~0.0.11.0 -Online
+Install-WindowsFeature -Name Remote-Desktop-Services, EnhancedStorage, File-Services, FS-FileServer
