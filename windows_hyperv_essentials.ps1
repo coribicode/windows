55b85d004e624bd014bddc57ae5015810f190ea3
davigalucio <82713318+davigalucio@users.noreply.github.com>
2025-01-07 05:05:14 -0300

    windows_hyperv_essentials.ps1

---
+++ windows_hyperv_essentials.ps1
@@ -0,0 +1,5 @@
+Add-WindowsCapability -Name ServerCore.AppCompatibility~~~~0.0.1.0 -Online
+Add-WindowsCapability -Name Tools.Graphics.DirectX~~~~0.0.1.0 -Online
+Add-WindowsCapability -Name NetFX3~~~~ -Online
+Add-WindowsCapability -Name Msix.PackagingTool.Driver~~~~0.0.1.0 -Online
+Add-WindowsCapability -Name Browser.InternetExplorer~~~~0.0.11.0 -Online
